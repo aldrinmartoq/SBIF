17031f5db303a006d17b938c1ea97fea42bcbfca
Aldrin Martoq <aldrin@martoq.cl>
2011-04-28 18:41:15 -0300

    Caché

--- api-sbif.php
+++ api-sbif.php
@@ -3,6 +3,16 @@
 $url = $_SERVER[REQUEST_URI];
 $url = str_replace("/huevapi/SBIF/api-sbif.php", "", $url);
 
-$url = "http://api.sbif.cl/api-sbif".$url."?apikey=b7e4ef2d3b8ed7e62a1fba8bcc50a97a536e6d98&formato=json";
+$cache = "cache/" . preg_replace("/[^a-zA-Z0-9\s]/", "-", $url);
+$cachetime = 120 * 60; // 2 horas
 
-echo file_get_contents($url);
+if (file_exists($cache) && (time() - $cachetime < filemtime($cache))) {
+	//echo file_get_contents($cache);
+} else {
+	$url = "http://api.sbif.cl/api-sbif".$url."?apikey=b7e4ef2d3b8ed7e62a1fba8bcc50a97a536e6d98&formato=json";
+	$data = file_get_contents($url);
+	$fp = fopen($cache, 'w');
+	fwrite($fp, $data);
+	fclose($fp);
+}
+echo file_get_contents($cache);
