bcc7b8fabe03f5878f93deb241cb3225eb79d1a2
Aldrin Martoq <aldrin@martoq.cl>
2011-05-02 16:59:30 -0300

    Quitando / (ó -) al nombre de archivo en caché.

--- api-sbif.php
+++ api-sbif.php
@@ -1,20 +1,19 @@
 <?php
 
 $url = $_SERVER[REQUEST_URI];
-$url = str_replace("/huevapi/SBIF/api-sbif.php", "", $url);
+$url = str_replace("/huevapi/SBIF/api-sbif.php/", "", $url);
 
-$cache = "cache/" . preg_replace("/[^a-zA-Z0-9\s]/", "-", $url);
+$cachefile = "cache/" . preg_replace("/[^a-zA-Z0-9\s]/", "-", $url);
 $cachetime = 120 * 60; // 2 horas
 
-if (file_exists($cache) && (time() - $cachetime < filemtime($cache))) {
-	//echo file_get_contents($cache);
+if (file_exists($cachefile) && (time() - $cachetime < filemtime($cachefile))) {
+	echo file_get_contents($cachefile);
 } else {
 	$apikey = trim(file_get_contents('.apikey'));
-	$url = "http://api.sbif.cl/api-sbif".$url."?apikey=".$apikey."&formato=json";
+	$url = "http://api.sbif.cl/api-sbif/".$url."?apikey=".$apikey."&formato=json";
 	$data = file_get_contents($url);
-	$fp = fopen($cache, 'w');
+	$fp = fopen($cachefile, 'w');
 	fwrite($fp, $data);
 	fclose($fp);
+	echo $data;
 }
-echo file_get_contents($cache);
-
