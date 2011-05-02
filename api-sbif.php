e376ed54d508d70a57ede78d8285c06d24a61687
Aldrin Martoq <aldrin@martoq.cl>
2011-05-02 17:09:17 -0300

    Cambio del caché a 30 mins; el .apikey ahora es un archivo php.

--- api-sbif.php
+++ api-sbif.php
@@ -4,12 +4,12 @@ $url = $_SERVER[REQUEST_URI];
 $url = str_replace("/huevapi/SBIF/api-sbif.php/", "", $url);
 
 $cachefile = "cache/" . preg_replace("/[^a-zA-Z0-9\s]/", "-", $url);
-$cachetime = 120 * 60; // 2 horas
+$cachetime = 60 * 60 * 0.5; // 60 seg * 60 min * 0.5 = 30 min
 
 if (file_exists($cachefile) && (time() - $cachetime < filemtime($cachefile))) {
 	echo file_get_contents($cachefile);
 } else {
-	$apikey = trim(file_get_contents('.apikey'));
+	include(".apikey");
 	$url = "http://api.sbif.cl/api-sbif/".$url."?apikey=".$apikey."&formato=json";
 	$data = file_get_contents($url);
 	$fp = fopen($cachefile, 'w');
